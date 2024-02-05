ed3425a2655ca39050300c99aaecf9d793222a6b
letsgooo <slavikshrot@gmail.com>
2024-02-05 16:27:22 +0100

    Updated test with asyncio

--- test_app.py
+++ test_app.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from app import app
@@ -5,6 +6,7 @@ from app import app
 client = TestClient(app)
 
 
+@pytest.mark.asyncio
 async def test_main_route():
     response = client.get("/app-status")
     assert response.status_code == 200
